43194dc6a4f29e4bb486ab753ed02307c8f40347
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-02 16:11:48 +0800

    Update Agecheck.js

--- Agecheck.js
+++ Agecheck.js
@@ -1,1 +1,14 @@
+// Prompt the user to enter their age
+let age = prompt("Please enter your age:");
 
+// Convert the input to a number
+age = Number(age);
+
+// Checking the age group
+if (age < 13) {
+    console.log("You are a child.");
+} else if (age >= 13 && age <= 19) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are an dult.");
+}
